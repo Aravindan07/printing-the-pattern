0395113068fe852c4895da61fb6ac08ffdee961d
Aravindan R <aravindanravi33@gmail.com>
2020-12-16 16:21:10 +0000

    fix: removed few files

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const chalk = require('chalk');
 
 console.log(chalk.yellowBright("This program will print the pattern based on your input\n"));
 
-let nValue = readlineSync.question('Enter a value for N\n');
+function printPatternFn(){
+  let option = readlineSync.question('\nHow to print the pattern\n a) normal\n b)reverse\n\n');
 
-let option = readlineSync.question('\nHow to print the pattern\n a) normal\n b)reverse\n\n');
+  if(option !== 'a' || option !== 'b' || Number.isInteger(option)){
+    console.log(option !== 'a');
+    console.log(option !== 'b');
+    console.log(Number.isInteger(option));
+    console.log(chalk.redBright("Invalid option please try again"));
+  }
 
 if(option === "a"){
   console.log('\n');
@@ -26,3 +32,17 @@ for(let i = 0; i < nValue; i++){
   console.log('\n');
 }
 }
+}
+
+
+
+let nValue = readlineSync.question('Enter a value for N\n');
+
+if(Number.isInteger(Number(nValue)) === false){
+  console.log(chalk.redBright("Invalid number please try again"));
+}
+else{
+  printPatternFn();
+}
+
+
